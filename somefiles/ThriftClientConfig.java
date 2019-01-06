fee0d9377e0bd2de0b73feee6fb3abf09ecefd04
ywb <18060840288@163.com>
2019-01-06 14:52:59 +0800

    'optimizeThriftClientConfig'

---
+++ somefiles/ThriftClientConfig.java
@@ -0,0 +1,14 @@
+package com.boostfield.hbj.portal.config;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ThriftClientConfig {
+  @Bean
+  public TProtocolFactory tProtocolFactory() {
+    return new TBinaryProtocol.Factory();
+  }
+}
